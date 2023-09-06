1fe72a99142f0a933114a7f938ee30861afe97a8
IvanSanchezDev <ivancampus2023@gmail.com>
2023-09-06 16:25:11 -0500

    feat: :sparkles: middleware para habilitar los cors

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { config } from 'dotenv'
 import { routes as routesAuth } from './routes/auth.routes.js'
+import cors from 'cors'
 config()
 
 
 const app=express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/auth',  routesAuth)
 
